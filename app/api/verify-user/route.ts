38343ad0cb4121a008308d57794e95ca4d708a11
Deepesh Chhetri <delta.10295@gmail.com>
2025-02-06 16:46:44 +0800

    fix compile error

--- app/api/verify-user/route.ts
+++ app/api/verify-user/route.ts
@@ -1,14 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
+interface CustomRequestCookie extends RequestCookie {
+  value: string;
+}
 
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest) {
-  const token = request.cookies.get("token");
+  const token: any = request.cookies.get("token");
+
+  if (!token) {
+    return NextResponse.json({ data: null });
+  }
+
+  const { value } = token;
 
   try {
-    const decoded = jwt.verify(token.value, process.env.JWT_SECRET!);
+    const decoded = jwt.verify(value || "", process.env.JWT_SECRET!);
 
     return NextResponse.json({ data: decoded });
   } catch (error) {
